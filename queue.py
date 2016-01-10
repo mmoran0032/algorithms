1e6d03bd8170ece09f56450b2d3aa5ab65c1976b
Mike Moran <mmoran0032@gmail.com>
2016-01-10 13:54:28 -0500

    starting queue object

---
+++ queue.py
@@ -0,0 +1,22 @@
+
+
+class Queue:
+
+    def __init__(self):
+        self._queue = []
+
+    def __len__(self):
+        return len(self.queue)
+
+    def __bool__(self):
+        return len(self) == 0
+
+    def enqueue(self, item):
+        self.queue.append(item)
+
+    def dequeue(self):
+        self.queue.pop(0)
+
+    @property
+    def queue(self):
+        return self._queue
